f006b8f5a420c22551da34f692a385564b63c4cd
AntonAlfsson <Anton.Alfsson@gmail.com>
2016-10-24 15:05:46 +0200

    Tillfällig konstruktor Customer popUp i Controller addCustomer

--- src/banksystem/Customer.java
+++ src/banksystem/Customer.java
@@ -3,5 +3,21 @@ package banksystem;
 // @author Anton
  
 public class Customer {
+    
+    private String name;
+    private long pNr;
+    
+    public Customer(String name, long pNr){
+        this.name = name;
+        this.pNr = pNr;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public long getpNr() {
+        return pNr;
+    }
+    
 }
--- src/banksystem/FXMLDocumentController.java
+++ src/banksystem/FXMLDocumentController.java
@@ -1,30 +1,54 @@
 package banksystem;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  *
  * @author asanilssonenglund
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
+
     @FXML
-    private void deleteCustomer(ActionEvent event) {
-        
+    private Button addCustomerButton;
+
+    @FXML
+    private void addCustomer(ActionEvent event) throws IOException {
+
+        Stage stage;
+        Parent root;
+        if (event.getSource() == addCustomerButton) {
+            stage = new Stage();
+            root = FXMLLoader.load(getClass().getResource("FXMLpopUp1.fxml"));
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initOwner(addCustomerButton.getScene().getWindow());
+            stage.showAndWait();
+        } else {
+            stage = (Stage) addCustomerButton.getScene().getWindow();
+            stage.close();
+        }
+
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         System.out.println("Hej svejs");
         System.out.println("nytt test för delgruppA");
-    }    
-    
+    }
+
 }
