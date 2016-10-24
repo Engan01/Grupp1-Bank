d6825de1e9358e25ec6a7537f7f1f986dd499ca4
AntonAlfsson <Anton.Alfsson@gmail.com>
2016-10-24 10:09:13 +0200

    Test

--- src/banksystem/BankSystem.java
+++ src/banksystem/BankSystem.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package banksystem;
 
 import javafx.application.Application;
@@ -15,6 +10,7 @@ import javafx.stage.Stage;
  *
  * @author asanilssonenglund
  */
+
 public class BankSystem extends Application {
     
     @Override
