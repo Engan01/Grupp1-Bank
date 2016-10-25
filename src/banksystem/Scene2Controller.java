6783e2474faed58282063fe027d46a0f38b947db
asanilssonenglund <asanilssonenglund@10.11.12.208>
2016-10-25 10:25:03 +0200

    lagt till fix:id

--- src/banksystem/Scene2Controller.java
+++ src/banksystem/Scene2Controller.java
@@ -9,6 +9,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -23,6 +28,71 @@ public class Scene2Controller implements Initializable {
     
     @FXML
     private Label name;
+    
+    @FXML
+    private Button editName;
+    
+    
+    @FXML
+    private Label ssn;
+    
+    @FXML
+    private ListView customerList;
+    
+    @FXML
+    private Button deleteCustomer;
+    
+    @FXML
+    private Button addCustomer;
+    
+    @FXML
+    private Label accountNr;
+    
+    @FXML
+    private Label balance;
+    
+    @FXML
+    private Label rate;
+    
+    @FXML
+    private Button deposit;
+    
+    @FXML
+    private Button withdraw;
+    
+    @FXML
+    private TextField amount;
+    
+    @FXML
+    private ListView transactionList;
+    
+    @FXML
+    private Button exportToFile;
+    
+    @FXML
+    private Label mainStatus;
+    
+    @FXML
+    private ChoiceBox transferFrom;
+    
+    @FXML
+    private ChoiceBox transferTo;
+    
+    @FXML
+    private TextField transferAmount;
+    
+    @FXML
+    private Label transferStatus;
+    
+    @FXML
+    private Button back;
+    
+    
+    
+    
+    
+    
+    
     
     
     
