f294e0ed6da6fb1f7f3a1ea1389d63c92627dcf6
AntonAlfsson <Anton.Alfsson@gmail.com>
2016-10-31 17:42:22 +0100

    tog bara bort en onödig import

--- src/banksystem/FXMLDocumentController.java
+++ src/banksystem/FXMLDocumentController.java
@@ -6,7 +6,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
