ca954458387b1b1d33e90d2fcba81a7e015c2283
Engan01 <exenglund@hotmail.com>
2016-11-02 15:31:51 +0100

    miniatyrändringar

--- src/banksystem/BankLogic.java
+++ src/banksystem/BankLogic.java
@@ -161,7 +161,7 @@ public class BankLogic {
 
 
 
- public boolean withdraw(long pNR, int accountNumber, double amount) {
+ public boolean withdraw(long pNR, int accountID, double amount) {
 
     
      
@@ -169,7 +169,7 @@ public class BankLogic {
             if (pNR == customersList.get(i).getPnr()) { // matchar personnummer
 
                 for (int j = 0; j < customersList.get(i).getAccountList().size(); j++) { // går in i matchande person och hämtar storlek på accountList
-                    if (accountNumber == customersList.get(i).getAccountList().get(j).getAccountNumber()) { // letar upp matchande kontonummer
+                    if (accountID == customersList.get(i).getAccountList().get(j).getAccountNumber()) { // letar upp matchande kontonummer
 
                         if ("CreditAccount".equals(customersList.get(i).getAccountList().get(j).getClass().getSimpleName())) { // kontrollerar om kontonummer är ett Kreditkonto
                             double currentBalance = customersList.get(i).getAccountList().get(j).getBalance(); // isf, hämtar belopp på kontot
@@ -198,10 +198,12 @@ public class BankLogic {
                             }
                             if (currentBalance >= 0) { // kontrollerar om det nya beloppet accepteras på sparkontot
                                 customersList.get(i).getAccountList().get(j).withdraw(amount);
-                                return true; // Accepteras
+                                return true;
+                                // Accepteras
                             } else {
                                 return false; // Accepteras inte
                             }
+                            
 
                         }
 
@@ -217,13 +219,13 @@ public class BankLogic {
     
     
     
-    public boolean deposit(long pNR, int accountNumber, double amount) {
+    public boolean deposit(long pNR, int accountID, double amount) {
 
         for (int i = 0; i < customersList.size(); i++) { // loppar igenom customerList
             if (pNR == customersList.get(i).getPnr()) { // matchar personnummer
 
                 for (int j = 0; j < customersList.get(i).getAccountList().size(); j++) { // går in i matchande person och hämtar storlek på accountList
-                    if (accountNumber == customersList.get(i).getAccountList().get(j).getAccountNumber()) { // letar upp matchande kontonummer
+                    if (accountID == customersList.get(i).getAccountList().get(j).getAccountNumber()) { // letar upp matchande kontonummer
 
                         if ("CreditAccount".equals(customersList.get(i).getAccountList().get(j).getClass().getSimpleName())) { // kontrollerar om kontonummer är ett Kreditkonto
                             double currentBalance = customersList.get(i).getAccountList().get(j).getBalance(); // isf, hämtar belopp på kontot
--- src/banksystem/Scene2Controller.java
+++ src/banksystem/Scene2Controller.java
@@ -40,7 +40,7 @@ public class Scene2Controller implements Initializable {
 
     @FXML
     private Label ssn;
-    
+
     @FXML
     private Label rate;
 
@@ -139,7 +139,10 @@ public class Scene2Controller implements Initializable {
                 c.getSelectedAccount(acountNR).addTransaction(false, amount2, c.getSelectedAccount(acountNR).getBalance());
                 String gg = Integer.toString(acountNR);
                 setTransactions();
-
+                if("SavingAccount".equals(c.getSelectedAccount(acountNR).getClass().getSimpleName())){
+                    rate.setText("2%");
+                }
+               
                 mainStatus.setText("Withdraw succesfull!");
             } else {
                 mainStatus.setText("Withdraw not possible!");
@@ -241,7 +244,7 @@ public class Scene2Controller implements Initializable {
         } else { // om användaen klickade cancel i popupen
             nameStatus.setText("No new name selected!");
         }
-        s.setToNull();  
+        s.setToNull();
     }
 
     @FXML
@@ -291,8 +294,7 @@ public class Scene2Controller implements Initializable {
             if (s1.isEmpty()) {
                 throw new NullPointerException();
             }
-            
-            
+
             String selectedAccountNumber = s1.replaceAll("[A-Za-z ]", "").trim();
             int selectedAccountNr = Integer.parseInt(selectedAccountNumber);
             Account selectedAccount = getThisObject().getSelectedAccount(selectedAccountNr);
@@ -341,8 +343,7 @@ public class Scene2Controller implements Initializable {
 
         b = BankLogic.getInstance();
         s = Singelton.getInstance();
-        
-        
+
         transactionObservable = FXCollections.observableArrayList();
         transactionList.setItems(transactionObservable);
         accountObservableList = FXCollections.observableArrayList();
@@ -354,7 +355,7 @@ public class Scene2Controller implements Initializable {
         accountList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                
+
                 Customer c = getThisObject();
                 try {
                     String str = (String) accountList.getSelectionModel().getSelectedItem();
@@ -364,18 +365,30 @@ public class Scene2Controller implements Initializable {
                         accountNr.setText(str);
                         setTransactions();
                         // konvertera ac
-                        
-                        
+
                         int accountNr2 = Integer.parseInt(str); // konverterar String till int
-                        
-                        System.out.println(c.getSelectedAccount(accountNr2).getClass().getSimpleName());
-                        if(.equals())
-                        
-                        System.out.println(accountNr2);
-                        
+                    
+                        if ("SavingAccount".equals(c.getSelectedAccount(accountNr2).getClass().getSimpleName())) {
+                            int ii = 0;
+                            for (Account a : getThisObject().getAccountList()) {
+                                if (a.getAccountNumber() == accountNr2) {
+                                    SavingAccount ss = (SavingAccount) a;
+                                    ii = ss.getnumberOfWithdraw();
+                                  
+                                }
+                            }
+                            if(ii > 0)
+                                rate.setText("2%");
+                            else
+                                rate.setText("0%");
+
+                        } else {
+                            rate.setText("0%");
+                        }
+                     
+
                         accountList.getSelectionModel().setSelectionMode(null);
-                        
-                        
+
                     }
                 } catch (NullPointerException e) {
                 }
