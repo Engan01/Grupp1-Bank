1c4e2a4551a979a484127f3cf8cd514e9b05f41b
AntonAlfsson <Anton.Alfsson@gmail.com>
2016-11-03 12:05:54 +0100

    asg

--- src/banksystem/FXMLDocumentController.java
+++ src/banksystem/FXMLDocumentController.java
@@ -235,7 +235,7 @@ public class FXMLDocumentController implements Initializable {
             for (int i = 0; i < lista1.length; i++) {
                 writer.write(lista1[i] + "\n");
             }
-            mainStatus.setText("File successfully exported.");
+            mainStatus.setText("CustomerList successfully exported to file");
             
         }catch(NullPointerException e){
         } catch (IOException e) {
