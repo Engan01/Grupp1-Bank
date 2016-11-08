61cf992b89e3b4150935b5069b30e64bdd0ee496
rafaldyewlski <rafal.dylewski0@gmail.com>
2016-11-08 12:34:08 +0100

    comments

--- src/banksystem/FXMLpopUp5.java
+++ src/banksystem/FXMLpopUp5.java
@@ -32,42 +32,42 @@ public class FXMLpopUp5 implements Initializable {
     private Label creditAccountLabel;
 
     @FXML
-    private void confirmPop5(ActionEvent event) {
-        ArrayList<Customer> arr = b.getCustomerList();
-        for(Customer ss : arr){
-            if(ss.getPnr() == b.getpNr())
-                ss.closeAccount(b.getAccountNr());
+    private void confirmPop5(ActionEvent event) {//confirm för delete account
+        ArrayList<Customer> arr = b.getCustomerList();//inicierar array "arr" av customer
+        for(Customer ss : arr){//för arrayen av objekt
+            if(ss.getPnr() == b.getpNr())//kolar på person nr
+                ss.closeAccount(b.getAccountNr());//stänger account genom metoden och kör uträkning
         }
         
         Stage stg = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stg.close();
+        stg.close();//stäng popup
        
     }
 
     @FXML
-    private void cancelPop5(ActionEvent event) {
-        Stage stg = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    private void cancelPop5(ActionEvent event) {//cancel för delete account
+        Stage stg = (Stage) ((Node) event.getSource()).getScene().getWindow();//hämtar popup
         stg.close();
-
+        //cancel knappen, stänger popup
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        b = BankLogic.getInstance();
-        ArrayList<Customer> arr = b.getCustomerList();
-        Account selectedAccount = null;
-        for(Customer c : arr){
-            if(c.getPnr() == b.getpNr()){
-                selectedAccount = c.getSelectedAccount(b.getAccountNr());
+        b = BankLogic.getInstance();//instance av backlogic med namn b
+        ArrayList<Customer> arr = b.getCustomerList();//inicierar  arraylist av customer
+        Account selectedAccount = null;// ingen accaunt är vald i list view
+        for(Customer c : arr){//för array lista av customer c
+            if(c.getPnr() == b.getpNr()){//om person nr stämer
+                selectedAccount = c.getSelectedAccount(b.getAccountNr());//hämntar accoubt nr för selected konto
             }
         }
         
-        balancePop5.setText(String.format("%.2f", selectedAccount.getBalance()) + " SEK");
-        interestRatePop5.setText(String.format("%.1f", selectedAccount.getInterest() * 100) + " %");
-        totalAmountPop5.setText(String.format("%.2f", selectedAccount.getTotalBalance()) + " SEK");
+        balancePop5.setText(String.format("%.2f", selectedAccount.getBalance()) + " SEK");//visar balance
+        interestRatePop5.setText(String.format("%.1f", selectedAccount.getInterest() * 100) + " %");//visar ränta
+        totalAmountPop5.setText(String.format("%.2f", selectedAccount.getTotalBalance()) + " SEK");//visar den totala balance
           
-        if(selectedAccount.getTotalBalance() < 0)
-           creditAccountLabel.setText(String.format("You have a debt of %.2f SEK", -1 * selectedAccount.getTotalBalance()));
+        if(selectedAccount.getTotalBalance() < 0)//om skullden finns
+           creditAccountLabel.setText(String.format("You have a debt of %.2f SEK", -1 * selectedAccount.getTotalBalance()));//skullden visas
            
     }
 
