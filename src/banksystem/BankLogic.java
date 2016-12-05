3f7763b6a54dc23f5252fd8f400eb7f022c51f79
AntonAlfsson <Anton.Alfsson@gmail.com>
2016-12-05 10:40:58 +0100

    test

--- src/banksystem/BankLogic.java
+++ src/banksystem/BankLogic.java
@@ -4,7 +4,8 @@ package banksystem;
 
 import java.util.ArrayList;
 
-public class BankLogic {
+public class BankLogic { //dsadsasadasdasdsdaas
+    //sdadsadasdDasa
 
     private ArrayList<Customer> customersList = new ArrayList<>(); // Lista med samtliga kunder
     private static BankLogic instance;
